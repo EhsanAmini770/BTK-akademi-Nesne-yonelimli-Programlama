ce4d7e73f5cdb2bc8e3ac21310983d24a9750586
Ehsanamini770 <u201118076@samsun.edu.tr>
2023-10-18 08:51:24 +0300

    Chapter_07 project has finished

---
+++ chapter_07 project/SmartHomeManagementSystem/SmartHomeManagementSystem/Appliance.cs
@@ -0,0 +1,44 @@
+﻿namespace SmartHomeManagementSystem
+{
+    public class Appliance : IControllable
+    {
+        private bool isOn;
+
+        public Appliance()
+        {
+            isOn = false; // By default, appliances are off
+        }
+
+        public void TurnOn()
+        {
+            if (!isOn)
+            {
+                isOn = true;
+                Console.WriteLine($"{GetType().Name} is turned on.");
+            }
+            else
+            {
+                Console.WriteLine($"{GetType().Name} is already on.");
+            }
+        }
+
+        public void TurnOff()
+        {
+            if (isOn)
+            {
+                isOn = false;
+                Console.WriteLine($"{GetType().Name} is turned off.");
+            }
+            else
+            {
+                Console.WriteLine($"{GetType().Name} is already off.");
+            }
+        }
+
+        public bool IsOn()
+        {
+            return isOn;
+        }
+    }
+
+}
